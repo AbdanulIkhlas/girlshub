2f7e3d00ab626d07140481fc5e1b7eb982e03e72
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-06-22 08:13:08 +0700

    membuat navbar view mobile

--- src/components/fragments/Navbar.jsx
+++ src/components/fragments/Navbar.jsx
@@ -1,15 +1,46 @@
+import { useState, useEffect, useRef } from "react";
 import Sidebar from "./Sidebar";
 import UILogo from "../ui/UILogo";
 
 const Navbar = () => {
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+  const sidebarRef = useRef(null);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
+
+  const handleClickOutside = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setSidebarVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSidebarVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isSidebarVisible]);
+
   return (
     <div>
-      <header className="w-full flex gap-1 items-center font-ubuntu border border-black px-5 py-5">
+      <header className="w-full font-ubuntu border border-black px-5 py-5">
         {/* view mobile */}
-        <section>
-          <img src="./svg/hamburger-icon.svg" alt="icon sidebar" />
+        <section className="flex gap-1 items-center">
+          <img
+            src="./svg/hamburger-icon.svg"
+            alt="icon sidebar"
+            onClick={toggleSidebar}
+            className="cursor-pointer"
+          />
           <UILogo />
-          <Sidebar />
+          <Sidebar isVisible={isSidebarVisible} sidebarRef={sidebarRef} />
         </section>
       </header>
     </div>
--- src/components/fragments/Sidebar.jsx
+++ src/components/fragments/Sidebar.jsx
@@ -3,9 +3,14 @@ import UILogo from "../ui/UILogo";
 import { Link } from "react-router-dom";
 import { Link as LinkScroll } from "react-scroll";
 
-const Sidebar = () => {
+const Sidebar = ({ isVisible, sidebarRef }) => {
   return (
-    <div className="absolute top-0 left-0 z-10 w-[80%] py-8 bg-white h-screen shadow-firstShadow">
+    <div
+      ref={sidebarRef}
+      className={`absolute top-0 left-0 z-10 w-[80%] py-8 bg-white h-screen shadow-firstShadow transition-transform ${
+        isVisible ? "translate-x-0" : "-translate-x-full"
+      }`}
+    >
       <UILogo customClassName="" />
       <h1 className="text-base mt-6 text-[#8391A1] font-jakartaSans px-4">
         MENU UTAMA
