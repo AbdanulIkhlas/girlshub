dc78995ae81a5d449a5dd2a15cc599817fc57b20
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-06-22 09:18:44 +0700

    konfigurasi warna

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,14 +11,18 @@ export default {
       //   hp: "360px",
       // },
       colors: {
-        primary: "#2C946C", //eucalyptus
+        bigText: "#282C4B",
         textColor: "#747582",
+        borderPink50: "#FF698D",
       },
       boxShadow: {
         firstShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.25)",
         cardShadow: "0 5px 16px 0 rgba(0, 0, 0, 0.15)",
         secondCardShadow:
           "rgba(0, 0, 0, 0.25) 0px 0.1em 0.1em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;",
+      },
+      backgroundImage: {
+        "gradient-to-right": "linear-gradient(to right, #FF698D, #FB3F6C)",
       },
       // borderRadius: {
       //   customRounded: "52% 48% 48% 52% / 50% 54% 46% 50% ",
